b819e0645a38a69d4db81f7f78387508fa85c21c
“Luke” <novelview9@gmail.com>
2021-07-21 09:00:55 +0900

    Update current font fixing

--- components/Text.jsx
+++ components/Text.jsx
@@ -46,7 +46,7 @@ function Text({ obj, addFixedData, isFull }) {
         let size = obj.avail_font_size * (isFull ? 1.2 : 1);
         if (frameHeight && height && ["S", "L"].includes(breakpoint)) {
             size *= (height / frameHeight) * 0.8;
-            setGoal({ fontSize: size, lineHeight: 140 });
+            setGoal({ fontSize: size, lineHeight: 130 });
         } else {
             setGoal({ fontSize: size, lineHeight: 150 });
         }
